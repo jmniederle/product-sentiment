Bug fix of taking sigmoid and softmax, bringing it all together

sentiment_model/evaluation.py
@@ -10,12 +10,12 @@ from sentiment_model.data_utils.tweet_dataset import TweetDataset, pad_batch
 from sentiment_model.model import SentimentNet
 from utils import get_project_root
 import numpy as np
+from sentiment_model.model_calibration import predict, CalibratedModel
+import torch.nn as nn
 
 
 def run_evaluation(
         batch_size: int = 16,
-        lr: float = 0.001,
-        epochs: int = 15,
         embedding_dim: int = 50,
         hidden_dim: int = 256,
         n_layers: int = 2,
@@ -24,7 +24,8 @@ def run_evaluation(
         dropout: float = 0.5,
         freeze_embed: bool = False,
         model_file="vivid-thunder-47/vivid-thunder-47-epoch-7.pth",
-        dataset="sent_ex"
+        dataset="sent_ex",
+        decision_bound=None,
 ):
 
     # Set device:
@@ -40,6 +41,7 @@ def run_evaluation(
 
     # Load data:
     test_dataset = TweetDataset(dataset=dataset, split="test", pretrained_vecs=glove_twitter)
+    valid_dataset = TweetDataset(split="valid", dataset="sent140_multi_class", pretrained_vecs=glove_twitter)
 
     # Create data loaders:
     test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False, collate_fn=pad_batch)
@@ -59,37 +61,52 @@ def run_evaluation(
     model.load_state_dict(checkpoint['model_state_dict'])
     model.to(device)
 
-    test_out = torch.tensor([]).to(device)
-    test_target = torch.tensor([]).to(device)
-
-    # Validation
-    model.eval()
-    with torch.no_grad():
-        for batch_idx, (data, target, text_lengths) in enumerate(test_loader):
-            data, target, text_lengths = data.to(device), target.to(device), text_lengths.to(device)
-
-            # Forward pass
-            output = model(data, text_lengths)
-            output = torch.sigmoid(output)
-            test_out = torch.cat((test_out, output))
-            test_target = torch.cat((test_target, target))
-
     if dataset == "sent_ex":
+        test_out = torch.tensor([]).to(device)
+        test_target = torch.tensor([]).to(device)
+        model.eval()
+        with torch.no_grad():
+            for batch_idx, (data, target, text_lengths) in enumerate(test_loader):
+                data, target, text_lengths = data.to(device), target.to(device), text_lengths.to(device)
+
+                # Forward pass
+                output = model(data, text_lengths)
+                output = torch.sigmoid(output)
+                test_out = torch.cat((test_out, output))
+                test_target = torch.cat((test_target, target))
+
+        acc = accuracy(test_out, test_target)
+        print(f"Test set accuracy: {acc}")
+        return test_out.cpu().numpy(), test_target.cpu().numpy(), model
+
+    elif dataset == "sent140":
+
+        print("Calibrating model")
+
+        # Calibrate model
+        CM = CalibratedModel(model)
+        CM.fit(valid_dataset, valid_dataset.get_y())
+
+        sm = nn.Softmax(dim=1)
+        test_out = torch.tensor([]).to(device)
+
+        # model.eval()
+        # with torch.no_grad():
+        #     for batch_idx, (data, target, text_lengths) in enumerate(test_loader):
+        #         data, target, text_lengths = data.to(device), target.to(device), text_lengths.to(device)
+        #
+        #         # Forward pass
+        #         output = model(data, text_lengths)
+        #         prob_out = sm(output)
+        #         test_out = torch.cat((test_out, prob_out))
+        #         test_target = torch.cat((test_target, target))
+        test_target = torch.tensor(test_dataset.get_y()).to(device)
+        test_out = torch.tensor(predict(CM, test_dataset, decision_bound)).to(device)
+
         acc = accuracy(test_out, test_target)
         print(f"Test set accuracy: {acc}")
 
-    elif dataset == "sent140":
-        mae = MAE(test_out, test_target)
-        print(f"MAE: {mae.item()}")
-        test_targets_bin = test_target.cpu().numpy()
-
-        out = test_out.cpu().reshape(-1).numpy()
-        out = out[test_targets_bin != 0.5]
-
-        test_targets_bin = test_targets_bin[test_targets_bin != 0.5]
-
-        print(np.mean((out >= 0.5).astype(float) == test_targets_bin))
-    return test_out.cpu().numpy(), test_target.cpu().numpy(), model
+        return test_out.cpu().numpy(), test_target.cpu().numpy(), model, CM
 
 
 if __name__ == "__main__":

sentiment_model/model.py
@@ -3,6 +3,7 @@ from torch.nn.utils.rnn import pack_padded_sequence, pad_packed_sequence
 import torch
 from torch.utils.data import DataLoader
 from sentiment_model.data_utils.tweet_dataset import pad_batch
+from tqdm import tqdm
 
 
 class SentimentNet(nn.Module):
@@ -77,7 +78,7 @@ class SentimentNet(nn.Module):
 
         return out
 
-    def predict_proba(self, X, y=None):
+    def predict_proba(self, X, multi_class=True):
         X_loader = DataLoader(X, batch_size=128, collate_fn=pad_batch, shuffle=False)
 
         predicted_probs = torch.tensor([]).to(self.device)
@@ -87,13 +88,18 @@ class SentimentNet(nn.Module):
         sm = nn.Softmax(dim=1)
 
         with torch.no_grad():
-            for batch_idx, (data, _, text_lengths) in enumerate(X_loader):
+            for batch_idx, (data, _, text_lengths) in tqdm(enumerate(X_loader)):
                 data, text_lengths = data.to(self.device), text_lengths.to(self.device)
 
                 output = self(data, text_lengths)
-                output = torch.sigmoid(output)
+                #output = torch.sigmoid(output)
 
-                prob_out = sm(output)
+                if multi_class:
+                    prob_out = sm(output)
+
+                else:
+                    prob_out = torch.sigmoid(output)
+
                 predicted_probs = torch.cat((predicted_probs, prob_out), dim=0)
 
         return predicted_probs.cpu().numpy()

sentiment_model/model_calibration.py
@@ -2,12 +2,16 @@ import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.calibration import calibration_curve
 from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import f1_score
+from sklearn.model_selection import KFold
+from torch.utils.data import Subset
+
 
 def plot_calib_curve(y_true, y_pred_prob, y_pred_prob_calib=None, bins=20):
-    fig, ax = plt.subplots(2,1, sharex=True, figsize=(10,10), gridspec_kw={'height_ratios':[3,1]})
+    fig, ax = plt.subplots(2, 1, sharex=True, figsize=(10, 10), gridspec_kw={'height_ratios': [3, 1]})
     prob_true, prob_pred = calibration_curve(y_true, y_pred_prob, n_bins=bins)
 
-    ax[0].plot([0,1], [0, 1], "k:", label="Perfectly calibrated")
+    ax[0].plot([0, 1], [0, 1], "k:", label="Perfectly calibrated")
     ax[0].plot(prob_pred, prob_true, "s-", label="Non-calibrated model")
     ax[0].set_ylabel("Fraction Positives")
     ax[0].set_xlabel("Mean predicted probability")
@@ -19,10 +23,10 @@ def plot_calib_curve(y_true, y_pred_prob, y_pred_prob_calib=None, bins=20):
     ax[0].set_ylim([-0.05, 1.05])
     ax[0].legend(loc="lower right")
 
-    ax[1].hist(y_pred_prob, range=(0,1), bins=20, histtype="step", lw=2, label="Non-calibrated model")
+    ax[1].hist(y_pred_prob, range=(0, 1), bins=20, histtype="step", lw=2, label="Non-calibrated model")
 
     if y_pred_prob_calib is not None:
-        ax[1].hist(y_pred_prob_calib, range=(0,1), bins=20, histtype="step", lw=2, label="Calibrated model")
+        ax[1].hist(y_pred_prob_calib, range=(0, 1), bins=20, histtype="step", lw=2, label="Calibrated model")
         ax[1].legend()
 
     ax[1].set_xlabel("Mean predicted probability")
@@ -60,4 +64,84 @@ class CalibratedModel:
         return self.calibrator.predict(model_preds)
 
 
+def one_hot_to_1d(arr):
+    return np.argmax(arr, axis=1)
 
+
+def one_hot_transform(labels_1d, unique_label_count="infer"):
+    if unique_label_count == 'infer':
+        unique_label_count = labels_1d.max() + 1
+
+    encoded = np.zeros((len(labels_1d), unique_label_count), dtype=int)
+    encoded[np.arange(len(labels_1d)), labels_1d] = 1
+    return encoded
+
+
+def predict(clf, dataset, boundary):
+    pred_probs = clf.predict_proba(dataset)
+
+    if type(boundary) == tuple:
+        return predict_flexible_boundary(pred_probs, boundary)
+
+    else:
+        return predict_boundary(pred_probs, boundary)
+
+
+def predict_boundary(pred_probs, boundary):
+    max_prob_idx = np.argmax(pred_probs, axis=1)
+    max_probs = np.take_along_axis(pred_probs, max_prob_idx.reshape(-1, 1), axis=1).reshape(-1)
+
+    predictions = np.zeros(len(pred_probs), dtype=int)
+    predictions[max_prob_idx > 0] = 2
+    predictions[max_probs < boundary] = 1
+    return predictions
+
+
+def predict_flexible_boundary(pred_probs, boundary):
+    b_0, b_2 = boundary
+    pred_probs = np.insert(pred_probs, 1, np.zeros(len(pred_probs)), axis=1)
+    # Set predicted probabilities to zero for non-maximal probs
+    pred_probs[~(pred_probs == np.max(pred_probs, keepdims=True, axis=1))] = 0
+    # Set predicted probabilities not reaching respective boundary to zero
+    pred_probs[pred_probs[:, [0, 1, 2]] < np.array([b_0, 0, b_2])] = 0
+    # Predicting neutral category where pos/neg not reached boundary
+    pred_probs[(pred_probs == 0).all(axis=1), 1] = 1
+    pred_probs[pred_probs > 0] = 1
+    return one_hot_to_1d(pred_probs.astype(int))
+
+
+def compute_decision_boundary(clf, dataset, boundaries, scorer=f1_score, scorer_args={"average": "macro"},
+                              return_scores=False, flexible=False):
+    pred_probs = clf.predict_proba(dataset)
+    labels = [y for _, y in dataset]
+    labels = one_hot_transform(np.array(labels))
+    scores = []
+    for b in boundaries:
+        if flexible:
+            preds = predict_flexible_boundary(pred_probs, b)
+
+        else:
+            preds = predict_boundary(pred_probs, b)
+
+        preds = one_hot_transform(preds, unique_label_count=labels.shape[1])
+        scores.append(scorer(labels, preds, **scorer_args))
+    if return_scores:
+        return boundaries[np.argmax(scores)], scores
+    else:
+        return boundaries[np.argmax(scores)]
+
+
+def dec_bound_opt(clf, dataset, boundaries, flexible=False, scorer=f1_score, scorer_args={"average": "macro"}):
+    cv_split = KFold(shuffle=True, n_splits=5)
+
+    test_scores = []
+
+    for train_split, test_split in cv_split.split(dataset):
+        train, test = Subset(dataset, train_split), Subset(dataset, test_split)
+        dec_bound = compute_decision_boundary(clf, train, boundaries, flexible=flexible, scorer=scorer,
+                                              scorer_args=scorer_args)
+        test_pred = predict(clf, test, dec_bound)
+        test_true = [y for _, y in test]
+        test_scores.append(scorer(test_true, test_pred, **scorer_args))
+
+    return test_scores